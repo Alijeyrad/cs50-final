d25c9dd8ab6a4fb928b6f745417db181a77a97e3
Alijeyrad <89392975+Alijeyrad@users.noreply.github.com>
2022-08-01 12:43:46 +0430

    home app with raw template

---
+++ home/views.py
@@ -0,0 +1,8 @@
+from django.shortcuts import render
+from django.urls import reverse
+from django.http import HttpResponseRedirect
+
+# Create your views here.
+
+def index(request):
+    return render(request, 'home/index.html')
--- panel/urls.py
+++ panel/urls.py
@@ -5,12 +5,12 @@ app_name = 'panel'
 
 urlpatterns = [
     path('', views.index, name='index'),
-    path('login_view', views.login_view, name='login_view'),
-    path('logout_view', views.logout_view, name='logout_view'),
-    path('register', views.register, name='register'),
-    path('panel', views.panel, name='panel'),
-    path('profile', views.profile, name='profile'),
-    path('profile_edit', views.profile_edit, name='profile_edit'),
-    path('change_password', views.change_password, name='change_password'),
-    path('change_picture', views.change_picture, name='change_picture')
+    path('/login_view', views.login_view, name='login_view'),
+    path('/logout_view', views.logout_view, name='logout_view'),
+    path('/register', views.register, name='register'),
+    path('/panel', views.panel, name='panel'),
+    path('/profile', views.profile, name='profile'),
+    path('/profile_edit', views.profile_edit, name='profile_edit'),
+    path('/change_password', views.change_password, name='change_password'),
+    path('/change_picture', views.change_picture, name='change_picture')
 ]
