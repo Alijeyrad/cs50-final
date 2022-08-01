2229e799357455df9e9f2ca7d0f117e73036aa85
Alijeyrad <89392975+Alijeyrad@users.noreply.github.com>
2022-08-02 01:42:44 +0430

    contact user based on email

--- home/views.py
+++ home/views.py
@@ -16,16 +16,23 @@ def contact(request):
         email = request.POST.get('email', False)
         subject = request.POST.get('subject', False)
         message = request.POST.get('message', False)
+    
+        new_contact = Contact.objects.create(name=name, email=email, subject=subject, message=message)
+
+        user_with_email = User.objects.get(email=email)
 
         try:
-            new_contact = Contact.objects.create(name=name, email=email, subject=subject, message=message)
             if request.user.is_authenticated:
                 user = User.objects.get(id=request.user.id)
                 new_contact.panel_user = user
                 new_contact.is_user = True
+            if not request.user.is_authenticated and user_with_email is not None:
+                new_contact.panel_user = user_with_email
+                new_contact.is_user = True
+
             new_contact.save()
         except:
-            messages.success(request, "Something happened to the message you sent. Send it again please.")
+            messages.error(request, "Something happened to the message you sent. Send it again please.")
             return HttpResponseRedirect(reverse('home:index'))
 
         messages.success(request, "Thank you for contacting us. We'll get back to you as soon as possible.")
--- panel/urls.py
+++ panel/urls.py
@@ -4,11 +4,11 @@ from . import views
 app_name = 'panel'
 
 urlpatterns = [
-    path('', views.index, name='index'),
+    path('login', views.index, name='index'),
     path('login_view', views.login_view, name='login_view'),
     path('logout_view', views.logout_view, name='logout_view'),
     path('register', views.register, name='register'),
-    path('panel', views.panel, name='panel'),
+    path('', views.panel, name='panel'),
     path('profile', views.profile, name='profile'),
     path('profile_edit', views.profile_edit, name='profile_edit'),
     path('change_password', views.change_password, name='change_password'),
