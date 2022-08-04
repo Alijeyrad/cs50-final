de8179283aa1c898d674f9851df959b25d81bc97
alijeyrad <alijeyrad@gmail.com>
2022-08-04 21:30:59 +0430

    test_neo view made

--- test_neo/views.py
+++ test_neo/views.py
@@ -1,6 +1,17 @@
 from django.shortcuts import render
+from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.hashers import check_password
+from django.contrib import messages
+from django.urls import reverse
+from django.utils.timezone import now
+from django.views.decorators.csrf import csrf_exempt
+from django.db import IntegrityError
+from django.contrib.auth.decorators import login_required
+from django.http import HttpResponseRedirect
+from .models import *
 
 # Create your views here.
 
+@login_required
 def index(request):
-    pass
+    return render(request, 'test_neo/build/index.html')
