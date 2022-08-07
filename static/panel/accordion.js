07a7442ce87afcde60cfcdaaa560f9b28461b912
alijeyrad <alijeyrad@gmail.com>
2022-08-07 21:36:22 +0430

    doctor profile buttons

--- static/panel/accordion.js
+++ static/panel/accordion.js
@@ -1,6 +1,12 @@
 function myFunction(id) {
   var x = document.getElementById(id);
+  
   if (x.className.indexOf("w3-show") == -1) {
+    // get the other one and close it
+    // before opening this one
+    for (let button of document.getElementsByClassName('btns')) {
+      button.classList.remove('w3-show');
+    }
     x.className += " w3-show";
   } else { 
     x.className = x.className.replace(" w3-show", "");
