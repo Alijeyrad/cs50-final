cfde746522d37ea1e7720c7122a4591362af581a
alijeyrad <alijeyrad@gmail.com>
2022-08-23 22:04:52 +0430

    rremoved a field from advice model

--- test_neo/models.py
+++ test_neo/models.py
@@ -9,7 +9,6 @@ class Advice(models.Model):
     test_id = models.CharField(max_length=255)
     content = models.TextField()
     timestamp = models.DateTimeField(auto_now_add=True)
-    read = models.BooleanField(default=False)
 
     def __str__(self):
-        return f"{self.doctor.username}'s advice for {self.test_owner.username}"
+        return f"{self.doctor.username}'s advice for {self.test_owner.username})"
