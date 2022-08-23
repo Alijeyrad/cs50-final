752291e5377913a34aeb77ee84e68d5a7d558e7c
alijeyrad <alijeyrad@gmail.com>
2022-08-24 01:35:24 +0430

    django-cors-headers removed

--- final_project/settings.py
+++ final_project/settings.py
@@ -34,7 +34,6 @@ INSTALLED_APPS = [
     'test_neo',
     'home',
     'panel',
-    "corsheaders", # developement only
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -46,7 +45,6 @@ INSTALLED_APPS = [
 AUTH_USER_MODEL = 'panel.User'
 
 MIDDLEWARE = [
-    "corsheaders.middleware.CorsMiddleware",
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -54,10 +52,6 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-]
-
-CORS_ALLOWED_ORIGINS = [
-    "http://localhost:3000",
 ]
 
 ROOT_URLCONF = 'final_project.urls'
